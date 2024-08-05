deac1a6458cb16ffc5d1afdd945e49c96a43073d
winjolu <winjolu@gmail.com>
2024-08-04 22:23:25 -0400

    detective jill

--- DetectiveJill.java
+++ DetectiveJill.java
@@ -10,42 +10,42 @@ public class DetectiveJill {
     private int solutionPerson;
 
     public DetectiveJill() {
-        this.jack = new AssistantJack(1); // initialize AssistantJack with a specific answer set
+        this.jack = new AssistantJack(1); // ! initialize AssistantJack with a specific answer set
     }
 
     public void initializeTheoryGeneration() {
-        // method to initialize theory generation
+        // ! method to initialize theory generation
         this.solutionWeapon = 1;
         this.solutionLocation = 1;
         this.solutionPerson = 1;
     }
 
     public void generateTheories() {
-        /* iterate through all possible combinations of weapon, location, and person */
-        for (int weapon = 1; weapon <= 6; weapon++) { // 6 possible weapons
-            for (int location = 1; location <= 10; location++) { // 10 possible locations
-                for (int person = 1; person <= 6; person++) { // 6 possible persons
-                    Theory theory = new Theory(weapon, location, person); // create new theory
-                    int result = jack.checkAnswer(theory); // check theory with AssistantJack
+        /* ! iterate through all possible combinations of weapon, location, and person */
+        for (int weapon = 1; weapon <= 6; weapon++) { // ! 6 possible weapons
+            for (int location = 1; location <= 10; location++) { // ! 10 possible locations
+                for (int person = 1; person <= 6; person++) { // ! 6 possible persons
+                    Theory theory = new Theory(weapon, location, person); // ! create new theory
+                    int result = jack.checkAnswer(theory); // ! check theory with AssistantJack
                     if (result == 0) {
-                        // correct theory found
+                        // ! correct theory found
                         this.solutionWeapon = weapon;
                         this.solutionLocation = location;
                         this.solutionPerson = person;
-                        return; // exit method as we found the correct theory
+                        return; // ! exit method as we found the correct theory
                     }
                 }
             }
         }
     }
 
-    public static void main(String[] args) {
-        DetectiveJill detective = new DetectiveJill();
-        detective.initializeTheoryGeneration();
-        detective.generateTheories();
-        System.out.println("Theory generation completed.");
-        System.out.println("Solution found: Weapon " + detective.solutionWeapon +
-                           ", Location " + detective.solutionLocation +
-                           ", Person " + detective.solutionPerson);
+    public static void main(String[] args) { // !
+        DetectiveJill detective = new DetectiveJill(); // !
+        detective.initializeTheoryGeneration(); // !
+        detective.generateTheories(); // !
+        System.out.println("Theory generation completed."); // !
+        System.out.println("Solution found: Weapon " + detective.solutionWeapon + // !
+                           ", Location " + detective.solutionLocation + // !
+                           ", Person " + detective.solutionPerson); // !
     }
 }
